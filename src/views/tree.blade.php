3be1d78733798e914d972375631b868d3b28fb8f
Raveendran3766 <raveendran@icore.net.in>
2025-01-23 11:04:38 +0530

    File Manager

--- src/views/tree.blade.php
+++ src/views/tree.blade.php
@@ -1,6 +1,6 @@
 <div class="m-3 d-block d-lg-none">
-  <h1 style="font-size: 1.5rem;">Laravel File Manager</h1>
-  <small class="d-block">Ver 2.0</small>
+  <h1 style="font-size: 1.5rem;">Teachers 2 You</h1>
+  <small class="d-block">File Manager</small>
   <div class="row mt-3">
     <div class="col-4">
       <img src="{{ asset('vendor/laravel-filemanager/img/152px color.png') }}" class="w-100">
